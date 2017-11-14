67c78f4927893314d06c6e407663d3235ab18773
Goldragon <goldragon227@gmail.com>
2017-11-13 18:32:21 -0800

    Init player

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Camera playerCamera;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	//Player input
+	void playerInput()
+	{
+		
+	}
+}
