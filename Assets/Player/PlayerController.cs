62b999a544ce2c180591858899e9e83660bea3e5
Goldragon <goldragon227@gmail.com>
2017-11-13 19:15:12 -0800

    Setup player movement

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -4,23 +4,87 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public Camera playerCamera;
+	private Camera playerCamera;
+	private Rigidbody playerRigidbody;
+	private PlayerData playerData;
+
+
+	KeyCode forward = KeyCode.W;
+	KeyCode right = KeyCode.D;
+	KeyCode left = KeyCode.A;
+	KeyCode back = KeyCode.S;
 
 	// Use this for initialization
 	void Start () 
 	{
-		
+		playerRigidbody = GetComponent<Rigidbody>();
+		playerData = GetComponent<PlayerData>();
+
+		playerCamera = GetComponentInChildren<Camera>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-
+		playerInput();
 	}
 
 	//Player input
 	void playerInput()
 	{
-		
+		bool bForward = Input.GetKey(forward);
+		bool bBack = Input.GetKey(back);
+		bool bRight = Input.GetKey(right);
+		bool bLeft = Input.GetKey(left);
+
+		if (bForward && bRight)
+		{
+			playerRigidbody.velocity =
+				new Vector3(1,0,1) * playerData.MovementSpeed * Time.deltaTime;
+		}
+		else if (bForward && bLeft)
+		{
+			playerRigidbody.velocity =
+				new Vector3(-1,0,1) * playerData.MovementSpeed * Time.deltaTime;
+		}
+		else if (bBack && bRight)
+		{
+			playerRigidbody.velocity =
+				new Vector3(1,0,-1) * playerData.MovementSpeed * Time.deltaTime;
+		}
+		else if (bBack && bLeft)
+		{
+			playerRigidbody.velocity =
+				new Vector3(-1,0,-1) * playerData.MovementSpeed * Time.deltaTime;
+		}
+		else if (bForward)
+		{
+			playerRigidbody.velocity = 
+				new Vector3(0,0,1) * playerData.MovementSpeed * Time.deltaTime;
+		}
+		else if (bBack)
+		{
+			playerRigidbody.velocity = 
+				new Vector3(0,0,-1) * playerData.MovementSpeed * Time.deltaTime;
+		}
+		else if (bRight)
+		{
+			playerRigidbody.velocity = 
+				new Vector3(1,0,0) * playerData.MovementSpeed * Time.deltaTime;
+		}
+		else if (bLeft)
+		{
+			playerRigidbody.velocity = 
+				new Vector3(-1,0,0) * playerData.MovementSpeed * Time.deltaTime;
+		}
+		else
+		{
+			playerRigidbody.velocity = new Vector3();
+		}
+
+		if (Input.GetKey(KeyCode.LeftShift))
+		{
+			playerRigidbody.velocity *= 2;
+		}
 	}
 }
---
+++ Assets/Player/PlayerData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour 
+{
+
+	[SerializeField]
+	private float _movementSpeed = 1;
+
+	public float MovementSpeed
+	{
+    	get 
+		{ 
+			return _movementSpeed; 
+		}
+    	set 
+		{ 
+			_movementSpeed = value; 
+		}
+	}
+
+}
