0041eee824f122aba17b9b75847e4ab8af86b2ae
randevpler <randallhines0@gmail.com>
2018-01-22 09:55:20 -0800

    Created Basic enemy 1

---
+++ Assets/PointTracking.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointTracking : MonoBehaviour {
+
+	public bool isTracking;
+	public GameObject objectToTrack;
+	public string tagOfObjectToTrack;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(isTracking)
+		{
+			transform.LookAt(objectToTrack.transform.position, transform.up);
+		}
+		
+	}
+}
